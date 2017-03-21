82b3cf1a64bf0cf4612977d30e626fd27783a59a
root <thomasericdean@gmail.com>
2017-03-20 21:02:01 -0400

    message

---
+++ html/transfer.php
@@ -0,0 +1,23 @@
+ <html>
+
+ <?php
+ require 'header.php';
+ ?>
+
+ <head>
+     <title>
+        Reports
+     </title>
+ </head>
+ <body>
+ <div class ="container">
+ Transfer
+  </div>
+
+ </div>
+ <?php
+ include_once('navBar.php');
+ ?>
+
+    </body>
+
