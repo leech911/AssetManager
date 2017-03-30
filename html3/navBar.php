8c6aecbb228f05afe8ad14269f7420b65bdd0600
Kyle Peterson <leech911@gmail.com>
2017-03-29 21:09:35 -0400

    yay

---
+++ html3/navBar.php
@@ -0,0 +1,44 @@
+<html>
+<?php 
+require 'header.php';
+?>
+<body>
+<div class="container" align="center">
+<h1> Asset Manager Alpha </h1>
+</div>
+<div class='container'>
+<ul id="slide-out" class="side-nav">
+
+      <li><div class="userView">
+        <div class="background">
+          <img src="../images/office.jpg">
+        </div>
+        <a href="#!user"><img class="circle" src="../images/box.jpg"></a>
+      </div></li>
+        <h5>  Asset Manager</h5>
+        <li><div class="divider"></div></li>
+
+      <li><a class="waves-effect" href="index.php">Home</a></li>
+      <li><a class="waves-effect" href="reports.php">Reports</a></li>
+      <li><div a class="divider"></div></li>
+  <li><a class="waves-effect" href="#modalReports">Tag Assets</a></li>
+     <li><a class="waves-effect" href="#modalReports">Transfer Assets</a></li>
+     <li><a class="waves-effect" href="#modalReports">Add New Asset</a></li>
+
+
+
+   </ul>
+    <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+
+
+</div>
+   </body>
+
+
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+
